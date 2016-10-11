c7306c3933a0f659bff5fee234ee0448c0c2439f
nickolay vasilishin <v.ns@hotmail.com>
2016-10-11 08:21:23 +0300

    г

---
+++ Traffic Kafka Producer/src/main/java/ru/nw/spiiras/nv/TrafficContainer.java
@@ -0,0 +1,8 @@
+package ru.nw.spiiras.nv;
+
+/**
+ * Created by Nick on 04.10.2016.
+ */
+interface TrafficContainer {
+    byte[] get();
+}
--- Traffic Kafka Producer/src/main/java/ru/nw/spiiras/nv/TrafficProvider.java
+++ Traffic Kafka Producer/src/main/java/ru/nw/spiiras/nv/TrafficProvider.java
@@ -4,5 +4,6 @@ package ru.nw.spiiras.nv;
  * Created by Nikolay_Vasilishin on 9/29/2016.
  */
 public interface TrafficProvider {
-
+    boolean hasNext();
+    TrafficContainer getNext();
 }
